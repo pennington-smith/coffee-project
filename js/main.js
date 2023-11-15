0eee0f719ab77085499498230bbb5c10cc01a383
Morgan <smorgannicole@gmail.com>
2023-11-15 08:52:52 -0600

    added functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+import { debounce } from './utils.js';
 const coffees = [
 	{ id: 1, name: "Light City", roast: "light" },
 	{ id: 2, name: "Half City", roast: "light" },
@@ -15,43 +16,111 @@ const coffees = [
 	{ id: 13, name: "Italian", roast: "dark" },
 	{ id: 14, name: "French", roast: "dark" },
 ];
+localStorage.setItem("coffees", JSON.stringify(coffees));
+let storedCoffees = localStorage.getItem("coffees");
 
-const createCoffeeElement = (coffee) => {
-	let tr = document.createElement("tr");
-	tr.classList.add("coffee");
+// to remove a coffee
+// localStorage.removeItem("coffees");
 
-	tr.innerHTML = `
-        <td>${coffee.id}</td>
-        <td>${coffee.name}</td>
-        <td>${coffee.roast}</td>
+const addCoffee = (coffeeName, roastType) => {
+	storedCoffees = JSON.parse(localStorage.getItem("coffees")) || [];
+	const newCoffee = { name: coffeeName, roast: roastType };
+	storedCoffees.push(newCoffee);
+	localStorage.setItem("coffees", JSON.stringify(newCoffee));
+	const storedCoffee = JSON.parse(localStorage.getItem("coffees"));
+	renderCoffeeElement(storedCoffee);
+};
+
+const renderCoffeeElement = (coffee) => {
+	const coffeeElement = document.createElement("div");
+	coffeeElement.classList.add(`d-flex`)
+	coffeeElement.innerHTML = `
+		<div class="col">
+			<p>${coffee.name}</p>
+		</div>
+		<div class="col">
+			<p>${coffee.roast}</p>
+		</div>
     `;
-	return tr;
+	document.querySelector("#coffees").appendChild(coffeeElement);
+	// const deleteBtn = coffeeElement.querySelector("button[data-delete]");
+	// deleteBtn.addEventListener("click", (e) => {
+	// 	coffeeElement.remove();
+	// });
+	// return coffeeElement;
 };
 
-const renderCoffees = (coffees, target) => {
-	// Clear tbody before inserting new rows
-	target.innerHTML = "";
-	for (let i = coffees.length - 1; i >= 0; i--) {
-		const coffeeElement = createCoffeeElement(coffees[i]);
-		target.appendChild(coffeeElement);
+const updateCoffees = (coffees) => {
+	document.querySelector("#coffees").innerHTML = "";
+	const roastSelectionInput = document.querySelector("#roast-selection");
+	const roastSelectionValue = roastSelectionInput.value;
+	const searchInput = document.querySelector("#search");
+	const searchValue = searchInput.value;
+
+	let filteredCoffees = coffees;
+
+	if (roastSelectionValue !== "all") {
+		filteredCoffees = filteredCoffees.filter((coffee) => {
+			return coffee.roast.toLowerCase().includes(roastSelectionValue.toLowerCase());
+		});
 	}
+
+	filteredCoffees = filteredCoffees.filter((coffee) => {
+		return coffee.name.toLowerCase().includes(searchValue.toLowerCase());
+	});
+
+	for (let coffee of filteredCoffees) {
+		renderCoffeeElement(coffee);
+	}
+
+	filteredCoffees = filteredCoffees.filter((coffee) => {
+		return coffee.name.toLowerCase().includes(searchValue.toLowerCase());
+	});
+
+	for (let coffee of filteredCoffees) {
+		renderCoffeeElement(coffee);
+	}
+
+	// const coffeesFragment = document.createDocumentFragment();
+	// for (let coffee of filteredCoffees) {
+	// 	coffeesFragment.appendChild(renderCoffeeElement(coffee));
+	// }
+	// document.querySelector("#coffees").appendChild(coffeesFragment);
+};
+const handleFilterEvents = (coffees) => {
+	const searchInput = document.querySelector("#search");
+	searchInput.addEventListener(
+		"input",
+		debounce((e) => {
+			if (searchInput.value.toLowerCase() === "the") {
+				return;
+			}
+			updateCoffees(coffees);
+		}, 500)
+	);
+
+	const roastSelectionInput = document.querySelector("#roast-selection");
+	roastSelectionInput.addEventListener("change", (e) => {
+		updateCoffees(coffees);
+	});
 };
 
-const updateCoffees = (e, target, selection) => {
-	e.preventDefault();
-	const selectedRoast = selection.value;
-	const filteredCoffees = coffees.filter((coffee) => coffee.roast === selectedRoast);
-	renderCoffees(filteredCoffees, target);
-};
-
-// IIFE
+// MAIN
 (() => {
-	const tbody = document.querySelector("#coffees");
-	const submitButton = document.querySelector("#submit");
-	const roastSelection = document.querySelector("#roast-selection");
-	renderCoffees(coffees, tbody, roastSelection);
-
-	submitButton.addEventListener("click", (e) => {
-		updateCoffees(e, tbody, roastSelection);
+	// window.addEventListener("load", () => {
+	// 	const storedCoffees = JSON.parse(localStorage.getItem("coffees")) || [];
+	// 	renderCoffeeElement(storedCoffees, document.querySelector("#coffees"));
+	// });
+	renderCoffeeElement(coffees)
+	updateCoffees(coffees);
+	handleFilterEvents(coffees);
+	const addBtn = document.querySelector("button[data-add]");
+	const nameInput = document.querySelector("#add-name");
+	const roastInput = document.querySelector(`#add-roast`);
+	addBtn.addEventListener("click", (e) => {
+		e.preventDefault();
+		addCoffee(nameInput.value, roastInput.value);
+		nameInput.value = "";
+		roastInput.value = "";
 	});
 })();
